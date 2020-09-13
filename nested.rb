19521f7c4a9d9ee0261e8c6cf27d557337bb3c25
Jessica Watts <jessperr182@hotmail.com>
2020-09-13 20:24:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -126,5 +126,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+     dennis_ritchies_language = "Assembly"
+  programmer_hash[:dennis_ritchie][:languages]
+  programmer_hash
 end
